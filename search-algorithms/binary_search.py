266f01cda827175ea1dd0b26f11c80e10d570010
kwahome <kevowahome@gmail.com>
2017-03-15 11:38:17 +0300

    Binary search algorithms

--- search-algorithms/binary_search.py
+++ search-algorithms/binary_search.py
@@ -1,28 +1,38 @@
 # -*- coding: utf-8 -*-
 
-# BINARY SEARCH ALGORITHM
-
+#============================================================================================================================================
+#
+# Author: Kelvin Wahome
+# Title: Binary Search Algorithm
+# Project: python-escapade
+# Package: search-algorithms
+#
+#
 # Binary search works on sorted arrays. 
 # Binary search begins by comparing the middle element of the array with the target value. 
 # If the target value matches the middle element, its position in the array is returned. 
 # If the target value is less than or greater than the middle element, the search continues in the lower or upper half of the array, 
 # respectively, eliminating the other half from consideration.
-
+#
 # Procedure:
-
+# ----------
+#
 # Given an array A of n elements with values or records A0 ... An−1, sorted such that A0 ≤ ... ≤ An−1, and target value T,
 # the following subroutine uses binary search to find the index of T in A.
-
+#
 #   - Set L to 0 and R to n − 1.
 #   - If L > R, the search terminates as unsuccessful.
 #   - Set m (the position of the middle element) to the floor (the largest previous integer) of (L + R) / 2.
 #   - If Am < T, set L to m + 1 and go to step 2.
 #   - If Am > T, set R to m – 1 and go to step 2.
 #   - Now Am = T, the search is done; return m.
-
+#
 # This iterative procedure keeps track of the search boundaries via two variables. 
 # Some implementations may place the comparison for equality at the end of the algorithm, resulting in a faster comparison loop but costing 
 # one more iteration on average.
+#
+#============================================================================================================================================
+
 
 import sys
 
@@ -57,7 +67,7 @@ def main():
 	print "\n"
 
 	for i in range (1,number+1):
-		search_list_item = raw_input("Please enter the item "+str(i)+" in your search list ")
+		search_list_item = raw_input("Please enter item "+str(i)+" in your search list ")
 		search_list.append(search_list_item)
 
 	print "\n"
