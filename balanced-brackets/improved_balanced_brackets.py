d7cc595d3a262f91b2099d0e4ae44936735acf6a
kwahome <kevowahome@gmail.com>
2017-03-17 20:42:15 +0300

    Comments and explanations

--- balanced-brackets/improved_balanced_brackets.py
+++ balanced-brackets/improved_balanced_brackets.py
@@ -51,37 +51,51 @@ def check_brackets(string_input, brackets_array):
 		if char in brackets_array:
 			# All opening brackets are at even positions in the brackets_array
 			if brackets_array.index(char)%2 is 0:
-				# if encountered character is an opening bracket, check for matching closing bracket in the balanced_array
+				# if encountered character is an opening bracket, check for matching closing bracket in balanced_array
 				if brackets_array[brackets_array.index(char)+1] in balanced_array:
-					# if a matching closing bracket is found in the 
+					# if a matching closing bracket is found in balanced_array,
+					# remove it as it matches the opening bracket encountered
 					balanced_array.remove(brackets_array[brackets_array.index(char)+1])				
 				else:
-					balanced_array.append(char)
-			else:
-				# check to see if there is an opening bracket in the balanced array
-				if brackets_array[brackets_array.index(char)-1] in balanced_array:
-					balanced_array.remove(brackets_array[brackets_array.index(char)-1])
-				else:
+					# if no matching closing bracket is found in balanced_array
+					# add the encountered opening bracket in our string into balanced_array
 					balanced_array.append(char)
 
+			# encountered character in the string is a closing bracket
+			# all closing brackets are on odd positions
+			else:
+				# check to see if there is a matching opening bracket in the balanced array
+				if brackets_array[brackets_array.index(char)-1] in balanced_array:
+					# if a matching opening bracket is found in balanced_array
+					# remove it as it matches with the closing bracket encountered
+					balanced_array.remove(brackets_array[brackets_array.index(char)-1])
+				else:
+					# if no matching opening bracket is found in balanced_array
+					# add the encountered closing bracket in our string into balanced_array
+					balanced_array.append(char)
+
+	# after looping through all characters in the string
+	# if the length of balanced_array is greater than 0 (i.e if balanced_array is not empty)
+	# return False as there is an unmatched bracket
 	if len(balanced_array) > 0:
 		return "False"
 
+	# else return true as all brackets "cancelled" each other out and thus are balanced
 	else:
 		return "True"
 	
 def main():
 
-	# # try statement
-	# try:
+	# try statement
+	try:
 
-	print "BALANCED BRACKET CHECK REPORT:"
-	print "\n"
+		print "BALANCED BRACKET CHECK REPORT:"
+		print "\n"
 
-	string_input = raw_input("Enter a string with parenthesis: ")
-	print "\n"
+		string_input = raw_input("Enter a string with parenthesis: ")
+		print "\n"
 
-	print check_brackets(string_input, brackets_array)
+		print check_brackets(string_input, brackets_array)
 
 	# Handle exceptions
 	except Exception:
