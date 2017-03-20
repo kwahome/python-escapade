572413644c414750fb1571a59ea3ccb9a81074c6
kwahome <kevowahome@gmail.com>
2017-03-20 18:43:55 +0300

    Function that checks whether any permutation ↴ of an input string is a palindrome

--- fun-with-problems/permutation_palindrome.py
+++ fun-with-problems/permutation_palindrome.py
@@ -24,16 +24,20 @@
 
 import sys
 
-def find_palindrome_permutation(string):
-	
+def find_permutation_palindrome(string):
+	# Dictionary to hold character frequencies
 	frequency = {}
-	palindrome = {}
 
+	# Traverse through the string character by character
 	for char in string:
+		# If a character in the string has been counted already, remove the counted character from frequency
+		# This means that for a palindrome to hold, we have to have the equal numbers of the same character
 		if char in frequency:
 			del frequency[char]
 		else:
 			frequency[char] = True
+
+	# At most one character may appear once and the string has permutation palindromes
 	return len(frequency) <= 1
 
 def main():
@@ -50,85 +54,14 @@ def main():
 
 	print "Does the string contain any permutation that is a palindrome?"
 	print "\n"
-	print find_palindrome_permutation(string)
+	print find_permutation_palindrome(string)
 
 	print "\n"
 
 if __name__ == "__main__":
 	
-	# try:
-	sys.exit(main())
+	try:
+		sys.exit(main())
 
-	# except Exception:
-	# 	print "An error has occured"
-# -*- coding: utf-8 -*-
-
-#============================================================================================================================================
-#
-# Author: Kelvin Wahome
-# Title: Permutation Palindrome
-# Project: python-escapade
-# Package: fun-with-problems
-#
-#
-# Write an efficient function that checks whether any permutation ↴ of an input string is a palindrome
-#
-# You can assume the input string only contains lowercase letters.
-#
-#  Examples:
-#
-#    "civic" should return True
-#    "ivicc" should return True
-#    "civil" should return False
-#    "livci" should return False
-# 
-#
-#============================================================================================================================================
-
-import sys
-
-def find_palindrome_permutation(string):
-
-	string = list(string)
-
-	permutation  = []
-
-	# walk towards the middle, from both sides
-	for left in range(len(string)):
-		
-		right = -left - 1
-
-		if string[right] == string[left]:
-			permutation.append(string[left])
-
-
-	if len(permutation) is 0:
-		return "False"
-	else:
-		return "The permuation '" + ''.join(permutation) + "' in the string is a palindrome"
-
-def main():
-
-	print "Palindrome in any permutation of a string"
-	print "------------------------------------------"
-
-	string = raw_input("Enter the string: ")
-
-	print "\n"
-	print "String input: "
-	print string
-	print "\n"
-
-	print "Does the string contain any permutation that is a palindrome?"
-	print "\n"
-	print find_palindrome_permutation(string)
-
-	print "\n"
-
-if __name__ == "__main__":
-	
-	# try:
-	sys.exit(main())
-
-	# except Exception:
-	# 	print "An error has occured"
+	except Exception:
+		print "An error has occured"
