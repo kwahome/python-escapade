7c457d0d7bd612936e362aff644b6b435678d377
kwahome <kevowahome@gmail.com>
2017-03-20 18:31:03 +0300

    Function that checks whether any permutation ↴ of an input string is a palindrome

--- fun-with-problems/permutation_palindrome.py
+++ fun-with-problems/permutation_palindrome.py
@@ -25,6 +25,68 @@
 import sys
 
 def find_palindrome_permutation(string):
+	
+	frequency = {}
+
+	for char in string:
+		if char in frequency:
+			del frequency[char]
+		else:
+			frequency[char] = True
+	return len(frequency) <= 1
+
+def main():
+
+	print "Palindrome in any permutation of a string"
+	print "------------------------------------------"
+
+	string = raw_input("Enter the string: ")
+
+	print "\n"
+	print "String input: "
+	print string
+	print "\n"
+
+	print "Does the string contain any permutation that is a palindrome?"
+	print "\n"
+	print find_palindrome_permutation(string)
+
+	print "\n"
+
+if __name__ == "__main__":
+	
+	# try:
+	sys.exit(main())
+
+	# except Exception:
+	# 	print "An error has occured"
+# -*- coding: utf-8 -*-
+
+#============================================================================================================================================
+#
+# Author: Kelvin Wahome
+# Title: Permutation Palindrome
+# Project: python-escapade
+# Package: fun-with-problems
+#
+#
+# Write an efficient function that checks whether any permutation ↴ of an input string is a palindrome
+#
+# You can assume the input string only contains lowercase letters.
+#
+#  Examples:
+#
+#    "civic" should return True
+#    "ivicc" should return True
+#    "civil" should return False
+#    "livci" should return False
+# 
+#
+#============================================================================================================================================
+
+import sys
+
+def find_palindrome_permutation(string):
 
 	string = list(string)
 
